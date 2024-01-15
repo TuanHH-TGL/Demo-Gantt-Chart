dbcdcbd8e535111d38a26a9bcc57624249047c8f
TuanHH-TGL <tuanhh.tglsol@gmail.com>
2024-01-15 10:20:23 +0700

    update validate

--- src/root-layout/use-validate-url.ts
+++ src/root-layout/use-validate-url.ts
@@ -1,21 +1,20 @@
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
+const allowedPaths = ["/gantt-schedule-timeline-calendar", "/gantt-task"];
+const allowedTypes = ["1", "2", "3"];
+
 const useValidateRoute = () => {
   const { pathname, search } = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (
-      pathname === "/" ||
-      (pathname !== "/gantt-schedule-timeline-calendar" &&
-        pathname !== "/gantt-task")
-    ) {
+    if (!allowedPaths.includes(pathname)) {
       navigate("/gantt-schedule-timeline-calendar?type=1");
       return;
     }
     const searchType = new URLSearchParams(search).get("type");
-    if (searchType === null || (searchType !== "1" && searchType !== "2")) {
+    if (searchType === null || !allowedTypes.includes(searchType)) {
       navigate(`${pathname}?${new URLSearchParams({ type: "1" })}`);
     }
   }, [pathname, navigate, search]);
